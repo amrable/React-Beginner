81f183b11cbadd1b06659fdc576b5263a763b837
amrfahmy <amrfahmy57@gmail.com>
2019-09-15 15:11:52 +0200

    add button to show the names

--- firstpage/src/App.js
+++ firstpage/src/App.js
@@ -21,9 +21,32 @@ class App extends Component {
 
   };
 
+  startHandler = () => {
+
+    this.setState({
+
+      persons: [
+        {
+          name: "Amr",
+          hobby: "N/A"
+        }, {
+          name: "Omar",
+          hobby: "N/A"
+        }, {
+          name: "Khaled",
+          hobby: "N/A"
+        }
+      ]
+    });
+
+  }
+
   render() {
     return (<div className="App">
       <h1>It is a React.js Page</h1>
+      <button onClick={this.startHandler}>
+        Start
+      </button>
       <Person name={this.state.persons[0].name} hobby={this.state.persons[0].hobby}/>
       <Person name={this.state.persons[1].name} hobby={this.state.persons[1].hobby}/>
       <Person name={this.state.persons[2].name} hobby={this.state.persons[2].hobby}/>
